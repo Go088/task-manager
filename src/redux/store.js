40983b059cd2c156a46b85a2d975e1afde182b30
Go088 <tapkharov@gmail.com>
2024-06-05 12:32:42 +0300

    update template

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,1 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import contactsReducer from "./contactsSlice";
-import filtersReducer from "./filtersSlice";
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filtersReducer,
-});
-
-export const store = configureStore({
-  reducer: rootReducer,
-});
