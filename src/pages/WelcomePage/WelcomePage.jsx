ad47cae17255fb726dbf0b133f9480b816c6f694
Olvet7 <volya.maslova@gmail.com>
2024-06-07 12:11:24 +0300

    check

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,10 @@
 const WelcomePage = () => {
-  return <div>Welcome to the Welcome Page!</div>;
+  return (
+    <>
+    <div>Welcome to the Welcome Page!</div>
+    <h1>Check</h1>
+    </>
+  )
 };
 
 export default WelcomePage;
