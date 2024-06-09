cd7b391d06370c1b11089affcb1c375fbc3aba93
Olvet7 <volya.maslova@gmail.com>
2024-06-09 15:30:32 +0300

    sprite (not all) + bg images

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+
+// "../."
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
