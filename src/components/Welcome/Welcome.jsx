6fc6124269dc1b6800433f63d7c74f53234dc820
Olvet7 <volya.maslova@gmail.com>
2024-06-09 18:12:16 +0300

    config conflict dirname src

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router-dom";
 import Container from "../Container/Container";
-import Icon from "../Icon/Icon";
+// import Icon from "../Icon/Icon";
 import css from "./Welcome.module.css";
 
-import WelcomeIcon from "@images/user.png";
-import WelcomeIconRetina from "@images/user@2x.png";
+// import WelcomeIcon from "@img/user.png";
+// import WelcomeIconRetina from "@img/user@2.png";
 
 
 const Welcome = () => {
   return (
     <Container>
-      <img
+      {/* <img
         srcSet={`${WelcomeIcon} 124w, ${WelcomeIconRetina} 248w`}
         src={`${WelcomeIcon}`}
         sizes="(min-width: 375px) 124px, (min-width: 768px) 162px, (min-width: 1440px) 162px"
         alt="Developers Avatar"
-      />
+      /> */}
 
-      <div>
+      {/* <div>
         <Icon
           id="icon-logo-task"
           width="12"
           height="16"
           className={css.icon_task_light}
         />
-      </div>
+      </div> */}
 
       <p>
         Supercharge your productivity and take control of your tasks with Task
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 
-// "../."
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@icons': resolve(__dirname, 'src/assets/icons'),
-      '@images': resolve(__dirname, 'src/assets/images'),
-    },
-  },
   build: {
     sourcemap: true,
-  },
+}
 });
