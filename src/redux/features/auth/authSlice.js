90757acdb3e9faa0e7e20d31bcfd269788ffe67c
KarinaShtankova <shtankovads@gmail.com>
2024-06-11 01:45:52 +0300

    add auth, redirect, Loader

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,25 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { registerUser, logIn, refreshUser } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
+    user: {
+      name: null,
+      email: null,
+    },
     token: null,
-    user: null,
+    isLoggedIn: false,
+    isRefreshing: false,
   },
-  reducers: {
-    setToken: (state, action) => {
-      state.token = action.payload;
-    },
-    setUser: (state, action) => {
-      state.user = action.payload;
-    },
-    clearAuth: (state) => {
-      state.token = null;
-      state.user = null;
-    },
-  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        // state.token = action.payload.token;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      }),
 });
-
-export const { setToken, setUser, clearAuth } = authSlice.actions;
 
 export default authSlice.reducer;
--- src/redux/features/auth/operations.js
+++ src/redux/features/auth/operations.js
@@ -1,1 +1,59 @@
-    
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://task-manager-r8dz.onrender.com/api";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (newUser, thunAPI) => {
+    try {
+      const response = await axios.post("/users/register", newUser);
+      //   setAuthHeader(response.data.token);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return thunAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (userData, thunkAPI) => {
+    try {
+      const response = await axios.post("/users/login", userData);
+      setAuthHeader(response.data.token);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    const {
+      auth: { token },
+    } = thunkAPI.getState();
+    setAuthHeader(token);
+    const response = await axios.get("/users/info");
+    console.log(response.data);
+    return response.data;
+  },
+  {
+    condition: (_, { getState }) => {
+      const {
+        auth: { token },
+      } = getState();
+      return token !== null;
+    },
+  }
+);
