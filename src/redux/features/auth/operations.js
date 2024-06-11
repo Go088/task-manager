0ae88d0cc57c991daca56686d68d2c4bcf88314f
KarinaShtankova <shtankovads@gmail.com>
2024-06-11 12:01:59 +0300

    add ErrorMessage and react-toast

--- src/redux/features/auth/operations.js
+++ src/redux/features/auth/operations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = "https://task-manager-r8dz.onrender.com/api";
 
@@ -16,11 +17,11 @@ export const registerUser = createAsyncThunk(
   async (newUser, thunAPI) => {
     try {
       const response = await axios.post("/users/register", newUser);
-      //   setAuthHeader(response.data.token);
-      console.log(response.data);
+      setAuthHeader(response.data.token);
+      toast.success(`${response.data.name}, your account successfully created!`);
       return response.data;
     } catch (error) {
-      console.log(error);
+      toast.error("Oops! Something went wrong. Please try again");
       return thunAPI.rejectWithValue(error.message);
     }
   }
@@ -32,10 +33,10 @@ export const logIn = createAsyncThunk(
     try {
       const response = await axios.post("/users/login", userData);
       setAuthHeader(response.data.token);
-      console.log(response.data);
+      toast.success(`Your account successfully logged in!`);
       return response.data;
     } catch (error) {
-      console.log(error);
+      toast.error("Oops! Something went wrong. Please try again");
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -45,9 +46,11 @@ export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
     const response = await axios.post("/users/logout");
     clearAuthHeader();
+    toast.success(`Your account successfully logged out!`);
     return response.data;
   } catch (error) {
     console.log(error);
+    toast.error("Oops! Something went wrong. Please try again");
     return thunkAPI.rejectWithValue(error.message);
   }
 });
