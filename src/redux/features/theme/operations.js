e517db0ebbbeb426c53e843e98d8816ae8ee8821
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-06-15 18:49:14 +0300

    sum

--- src/redux/features/theme/operations.js
+++ src/redux/features/theme/operations.js
@@ -1,21 +1,21 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-// // import toast from "react-hot-toast";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// import toast from "react-hot-toast";
 
-// axios.defaults.baseURL = "https://task-manager-r8dz.onrender.com/api";
+axios.defaults.baseURL = "https://task-manager-r8dz.onrender.com/api";
 
-// export const fetchTheme = createAsyncThunk(
-//   "users/theme",
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await axios.get("/users/theme");
-//       return response.data;
-//     } catch (error) {
-//         return thunkAPI.rejectWithValue(error.message)
-//     }
-//   }
-// );
+export const fetchTheme = createAsyncThunk(
+  "users/theme",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/users/theme");
+      return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+  }
+);
 
-// export const changeTheme = createAsyncThunk(
-//     "users/theme",
-// ) 
+export const changeTheme = createAsyncThunk(
+    "users/theme",
+) 
--- src/redux/features/theme/themeSlice.js
+++ src/redux/features/theme/themeSlice.js
@@ -1,32 +1,36 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { fetchTheme } from "./operations";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTheme } from "./operations";
 
-// const initialState = {
-//   theme: "dark",
-//   loading: false,
-//   error: false,
-// };
+const initialState = {
+  theme: "dark",
+  loading: false,
+  error: false,
+};
 
-// const themeSlice = createSlice({
-//   name: "theme",
-//   initialState,
-//   extraReducers: (builder) =>
-//     builder
-//       .addCase(fetchTheme.pending, (state) => {
-//         state.error = false;
-//         state.loading = true;
-//       })
-//       .addCase(fetchTheme.fulfilled, (state, action) => {
-//         state.theme = action.payload;
-//         state.loading = false;
-//       })
-//       .addCase(fetchTheme.rejected, (state) => {
-//         state.error = true;
-//         state.loading = false;
-//       }),
-// });
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchTheme.pending, (state) => {
+        state.error = false;
+        state.loading = true;
+      })
+      .addCase(fetchTheme.fulfilled, (state, action) => {
+        state.theme = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchTheme.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      }),
+});
 
-// export const { changeTheme } = themeSlice.actions;
+export const { changeTheme } = themeSlice.actions;
 
-// export default themeSlice.reducer;
+export default themeSlice.reducer;
+
+// changeTheme(state, action) {
+//   state.theme = action.payload;
+// },
 
