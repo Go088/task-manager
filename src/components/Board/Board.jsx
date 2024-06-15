b33b407540757a10f345bfa5af77e2ad157d76b8
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-06-15 11:43:01 +0300

    summary

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,25 +2,42 @@ import { useDispatch } from 'react-redux';
 import { deleteBoard } from '../../redux/features/boards/operations';
 import css from './Board.module.css';
 import Icon from '../Icon/Icon';
+import EditForm from '../EdiForm/EditForm';
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
 
-export const Board = ({ id, title ,icons}) => {
+
+export const Board = ({ _id, title ,icon}) => {
 const dispatch = useDispatch();
-const handleDelete = () => dispatch(deleteBoard(id));
-const handlEdit =()=>dispatch(deleteBoard(id));
-const titleSVG = `icon-${icons.trim()}`
+const handleDelete = () => dispatch(deleteBoard(_id));
+const handlEdit =()=>dispatch(deleteBoard(_id));
+const titleSVG = `icon-${icon.trim()}` 
+const [editIsOpen, setIsOpen] = useState(false);
 
+useEffect(() => {
+Modal.setAppElement("#root");
+}, []);
+
+const handleOpen = () => setIsOpen(true);
+
+const closeModal = () => {
+setIsOpen(false);
+};
 return (
     <div className={css.wrapper}>
     <Icon id={titleSVG} width="18" height="18" className={css.titleSVG}/>
     <h2 className={css.text}>{title}</h2>
     <div className={css.btnList}>
-    <button type="button" className={css.button} onClick={handleDelete}>
+    <button type="button" className={css.button} onClick={handleOpen}>
     <Icon id="icon-pencil" width="16" height="16" className={css.LogoSVG}/>
     </button>
-    <button type="button" className={css.button} onClick={handlEdit}>
+    <button type="button" className={css.button} onClick={handleDelete}>
     <Icon id="icon-trash" width="16" height="16" className={css.LogoSVG}/>
     </button>
     </div>
+    {editIsOpen && (
+        <EditForm isOpen={editIsOpen} onRequestClose={closeModal}  onclick={handlEdit} _id={_id}/>
+    )}
     </div>
 );
 };
