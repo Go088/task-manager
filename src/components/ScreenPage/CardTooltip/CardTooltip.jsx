4e5bdcfeff1d50e353aaea4c7212636aeadbc701
KarinaShtankova <shtankovads@gmail.com>
2024-06-18 13:35:31 +0300

    fix card tooltip

--- src/components/ScreenPage/CardTooltip/CardTooltip.jsx
+++ src/components/ScreenPage/CardTooltip/CardTooltip.jsx
@@ -10,9 +10,8 @@ import {
 import { selectTheme } from "../../../redux/features/theme/selectors";
 
 export default function CardTooltip({
-  card: { _id, title, description, priority, deadline },
+  card: { _id, title, description, priority, deadline, owner },
 }) {
-
   const board = useSelector(selectBoard);
   const columns = board.columns;
   const themeType = useSelector(selectTheme);
@@ -32,35 +31,36 @@ export default function CardTooltip({
         data,
         _id: columnId,
       };
-
       dispatch(addCard(cardData));
     };
   };
 
   return (
     <ul className={clsx(s.ul, s[themeType])}>
-      {columns.map((column) => {
-        const columnId = column._id;
-        return (
-          <li className={s.item} key={columnId}>
-            <button
-              className={clsx(s.button)}
-              onClick={handleClick(_id, column._id)}
-              aria-label="button to change card's column"
-            >
-              <div className={clsx(s.title, s[themeType])}>
-                {column.title}
-              </div>
-              <Icon
-                className={clsx(s.icon, s[themeType])}
-                width="16px"
-                height="16px"
-                id="icon-arrow-circle-broken-right"
-              />
-            </button>
-          </li>
-        );
-      })}
+      {columns
+        .filter((column) => column._id !== owner)
+        .map((column) => {
+          const columnId = column._id;
+          return (
+            <li className={s.item} key={columnId}>
+              <button
+                className={clsx(s.button)}
+                onClick={handleClick(_id, column._id)}
+                aria-label="button to change card's column"
+              >
+                <div className={clsx(s.title, s[themeType])}>
+                  {column.title}
+                </div>
+                <Icon
+                  className={clsx(s.icon, s[themeType])}
+                  width="16px"
+                  height="16px"
+                  id="icon-arrow-circle-broken-right"
+                />
+              </button>
+            </li>
+          );
+        })}
     </ul>
   );
 }
