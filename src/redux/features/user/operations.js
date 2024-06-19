2f7dc9c00ef875f6d7aa5eb54be449ecdcbc5d35
Vasyl-Djachuk <145469812+Vasyl-Djachuk@users.noreply.github.com>
2024-06-19 22:12:35 +0800

    fsdfsd

--- src/redux/features/user/operations.js
+++ src/redux/features/user/operations.js
@@ -11,11 +11,48 @@ export const updateUser = createAsyncThunk(
   "user/updateUser",
   async (userData, thunkAPI) => {
     try {
-      const response = await axios.put("/users/edit", userData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+      const response = await axios.put("/users/edit", userData);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getUserAvatar = createAsyncThunk(
+  "user/getUserAvatar",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/users/avatar");
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const editUserAvatar = createAsyncThunk(
+  "user/editUserAvatar",
+  async (photo, thunkAPI) => {
+    try {
+      setAuthHeader(response.data.token);
+      const response = await axios.put("/users/edit-avatar", photo, {
+        headers: { "Content-Type": "multipart/form-data" },
       });
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+export const refreshUser = createAsyncThunk(
+  "user/refreshUser",
+  async (formData, thunkAPI) => {
+    try {
+      const response = await axios.get("/users/info");
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
