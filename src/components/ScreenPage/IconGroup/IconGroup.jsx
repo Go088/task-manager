0839e295ffd0c4eaa6de291a8ec1da17941b58f0
KarinaShtankova <shtankovads@gmail.com>
2024-06-19 22:28:35 +0300

    add delete modals and update tooltips

--- src/components/ScreenPage/IconGroup/IconGroup.jsx
+++ src/components/ScreenPage/IconGroup/IconGroup.jsx
@@ -1,31 +1,32 @@
-import Icon from "../../Icon/Icon";
-import css from "./IconGroup.module.css";
 import clsx from "clsx";
-
-import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import Modal from "react-modal";
-import EditCard from "../../EditCard/EditCard";
-import { deleteCard } from "../../../redux/features/boardss/operations";
-import { Tooltip } from "react-tooltip";
-import CardTooltip from "../CardTooltip/CardTooltip";
 
-const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
-  const [modalIsOpen, setIsOpen] = useState(false);
+import Modal from "react-modal";
+import { Tooltip } from "react-tooltip";
+
+import Icon from "../../Icon/Icon";
+import EditCard from "../../EditCard/EditCard";
+import CardTooltip from "../CardTooltip/CardTooltip";
+import CardDeleteModal from "../CardDeleteModal/CardDeleteModal";
+import css from "./IconGroup.module.css";
+import DeadlineTooltip from "../DeadlineTooltip/DeadlineTooltip";
+
+const IconGroup = ({ theme, bellIconColor, card }) => {
+  const [editIsOpen, setEditIsOpen] = useState(false);
+  const [deleteIsOpen, setDeleteIsOpen] = useState(false);
 
   useEffect(() => {
     Modal.setAppElement("#root");
   }, []);
 
-  const dispatch = useDispatch();
-
-  const handleDelete = () => {
-    console.log();
-    dispatch(deleteCard(_id));
+  const handleOpenEdit = () => setEditIsOpen(true);
+  const closeEditModal = () => {
+    setEditIsOpen(false);
   };
-  const handleOpen = () => setIsOpen(true);
-  const closeModal = () => {
-    setIsOpen(false);
+
+  const handleOpenDlete = () => setDeleteIsOpen(true);
+  const closeDeleteModal = () => {
+    setDeleteIsOpen(false);
   };
 
   const whiteTheme = theme === "dark" ? "" : "white";
@@ -33,12 +34,17 @@ const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
     <>
       <ul className={css.iconList}>
         <li className={clsx(css.li, css.margin)}>
-          <Icon
-            className={clsx(css.iconBell, css[theme], css[bellIconColor])}
-            width="16px"
-            height="16px"
-            id="icon-bell"
-          />
+          <a id={`not-clickable-${card._id}`}>
+            <Icon
+              className={clsx(css.iconBell, css[theme], css[bellIconColor])}
+              width="16px"
+              height="16px"
+              id="icon-bell"
+            />
+          </a>
+          <Tooltip anchorSelect={`#not-clickable-${card._id}`} clickable>
+            <DeadlineTooltip />
+          </Tooltip>
         </li>
 
         <li className={css.li}>
@@ -53,13 +59,13 @@ const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
             </button>
           </a>
           <Tooltip anchorSelect={`#clickable-${card._id}`} clickable>
-            <CardTooltip theme={theme} card={card} />
+            <CardTooltip card={card} />
           </Tooltip>
         </li>
         <li className={css.li}>
           <button
             className={css.button}
-            onClick={handleOpen}
+            onClick={handleOpenEdit}
             type="button"
             aria-label="Edit the card"
           >
@@ -74,7 +80,7 @@ const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
         <li className={css.li}>
           <button
             className={css.button}
-            onClick={handleDelete}
+            onClick={handleOpenDlete}
             type="button"
             aria-label="Delete the card"
           >
@@ -87,10 +93,17 @@ const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
           </button>
         </li>
       </ul>
-      {modalIsOpen && (
+      {editIsOpen && (
         <EditCard
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
+          isOpen={editIsOpen}
+          onRequestClose={closeEditModal}
+          card={card}
+        />
+      )}
+      {deleteIsOpen && (
+        <CardDeleteModal
+          isOpen={deleteIsOpen}
+          onRequestClose={closeDeleteModal}
           card={card}
         />
       )}
@@ -98,3 +111,4 @@ const IconGroup = ({ theme, bellIconColor, card, card: { _id } }) => {
   );
 };
 export default IconGroup;
+
